85dbcc67745f159d231e942496abb5a52e81a3ec
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2024-10-11 02:27:54 +0700

    refactor: add key and remove unused imports

--- components/categoryList/CategoryList.tsx
+++ components/categoryList/CategoryList.tsx
@@ -1,6 +1,5 @@
 import { TCategory } from "@/hooks/articles/articlesType";
 import { useGetCategory } from "@/hooks/articles/hooks";
-import React, { FC } from "react";
 import { CategoryItem } from "../categoryItem/CategoryItem";
 
 export const CategoryList = () => {
@@ -11,7 +10,7 @@ export const CategoryList = () => {
       {categories?.data.map((category: TCategory) => (
         <div key={category.id} className="p-3 border-b">
           <h3 className="text-lg font-bold">{category.name}</h3>
-          <CategoryItem category_id={category.id} />
+          <CategoryItem category_id={category.id} key={category.id} />
         </div>
       ))}
     </div>
