bc4c987450206305fc532c14205a6afd413d0969
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2024-10-11 02:31:48 +0700

    rafactor: add key to category item

--- components/categoryItem/CategoryItem.tsx
+++ components/categoryItem/CategoryItem.tsx
@@ -8,7 +8,7 @@ export const CategoryItem: FC<{ category_id: number }> = ({ category_id }) => {
   return (
     <div>
       {data?.data.data.map((article) => (
-        <Link href={`article/${article.slug}/${article.id}`}>
+        <Link href={`article/${article.slug}/${article.id}`} key={article.id}>
           <div key={article.id} className="p-2 rounded-md border my-2">
             <p key={article.id}>{article.title}</p>
           </div>
